167d7de99f47cf49fb8ed287c3ab96d475d5cb72
Derrick Helgeson <dkmk1020@gmail.com>
2021-07-29 09:14:34 -0700

    update

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-//generate the code
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "!@#$^&%*()+=-[]{}|:<>?,.";
+var numbers = "1234567890";
+
 function generate() {
+  let code = document.getElementById("slider").value;
 
-let complexity = document.getElementById("slider").value;
+  var values = "";
+  values += lower;
+  values += upper;
+  values += special;
+  values += numbers;
 
-let values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&*?";
+  let password = "";
 
-let password = "";
+  for (i = 0; i <= code; i++) {
+    password =
+      password +
+      values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
 
-for(i = 0; i <= complexity; i++) {
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-}
+  document.getElementById("display").value = password;
 
-document.getElementById("display").value = password;
-
-//previous passwords
-
-document.getElementById("lastPasswords").innerHTML += password + "<br />";
+  document.getElementById("lastPasswords").innerHTML += password + "<br />";
 }
 
 document.getElementById("length").innerHTML = "length: 20";
 
-document.getElementById("slider").oninput = function(){
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "lenght: " + document.getElementById("slider").value;
-    }
-    else{
-        document.getElementById("lenght").innerHTML = "length: 1";
-    }
-}
-//copy code
-function copyPassword(){
-    document.getElementById("display").select();
-   
-    document.execCommand("copy");
-   
-    alert("password copied");
+document.getElementById("slider").oninput = function () {
+  if (document.getElementById("slider").value > 5) {
+    document.getElementById("length").innerHTML =
+      "lenght: " + document.getElementById("slider").value;
+  } else {
+    document.getElementById("lenght").innerHTML = "length: 1";
+  }
+};
+
+function copyPassword() {
+  document.getElementById("display").select();
+
+  document.execCommand("copy");
+
+  alert("password copied");
 }
